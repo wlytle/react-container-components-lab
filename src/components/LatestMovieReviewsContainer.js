cc07256c492bc6a605efde064a30732ebb8a75ce
wlytle <wlytle89@gmail.com>
2020-12-16 15:03:56 -0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,9 +1,36 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+import React, { Component } from "react";
+import "isomorphic-fetch";
+import MovieReviews from "./MovieReviews";
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = "dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ";
+const URL =
+  "https://api.nytimes.com/svc/movies/v2/reviews/all.json?" +
+  `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+  state = {
+    reviews: [],
+  };
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then((resp) => resp.json())
+      .then((reviews) => {
+        this.setState({
+          reviews: reviews.results,
+        });
+      });
+  }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,22 @@
 // Code MovieReviews Here
+import React from "react";
+import Review from "./Review";
+
+const renderReviews = (reviews) => {
+  return reviews.map((review) => {
+    return (
+      <Review
+        key={review.id}
+        headline={review.headline}
+        byline={review.byline}
+        summary={review.summary_short}
+      />
+    );
+  });
+};
+
+const MovieReviews = ({ reviews }) => {
+  return <div className="review-list">{renderReviews(reviews)}</div>;
+};
+
+export default MovieReviews;
